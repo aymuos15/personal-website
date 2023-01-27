144b8a02f8f2f50609181222b8c27e3e7a3e757d
finnala <finnaladev@protonmail.com>
2023-01-27 10:42:16 +0000

    feat: change wording of about section

--- src/components/about.svelte
+++ src/components/about.svelte
@@ -2,4 +2,5 @@
     hi i'm finnala, a self-taught developer interested in computers. currently i'm working as a developer advocate for
     a stealth startup.
 </p>
-<p>currently i'm exploring quant, ai/ml, functional programming, and anything else that seems interesting.</p>
+<p>i'm exploring quant, ai, and lower-level programming.</p>
+<p>i want to learn and help others learn about interesting areas of computer science outside of web development.</p>
