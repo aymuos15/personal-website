fd9e290cb5a6476f715e2372ad3d7ab6ef38a3a2
finnala <finnaladev@protonmail.com>
2023-02-03 11:48:36 +0000

    feat: rm svelte src

--- src/components/about.svelte
+++
@@ -1,6 +0,0 @@
-<p>
-    hi i'm finnala, a self-taught developer interested in computers. currently i'm working as a developer advocate for
-    a stealth startup.
-</p>
-<p>i'm exploring quant, ai, and lower-level programming.</p>
-<p>i want to learn and help others learn about interesting areas of computer science outside of web development.</p>
--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import "../styles/global.css";
-    import Footer from "../components/footer.svelte";
-</script>
-
-<slot></slot>
-<Footer />
